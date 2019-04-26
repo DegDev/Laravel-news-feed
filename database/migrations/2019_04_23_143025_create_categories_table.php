8d3acec177c45d8a1cf4aadf0037404aecfdfcd4
dmitri__puscas <dmitri@innoshore.com>
2019-04-26 17:19:22 +0300

    Category Migration

--- database/migrations/2019_04_23_143025_create_categories_table.php
+++ database/migrations/2019_04_23_143025_create_categories_table.php
@@ -16,7 +16,7 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name',50);
-            $table->string('slug',50);
+            $table->string('slug',50)->unique()->nullable();;
             $table->timestamps();
         });
     }
